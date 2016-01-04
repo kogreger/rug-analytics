6e2a4c90ad991335952db3781afef46f0576a3c4
Konstantin Greger <kogreger@gmail.com>
2016-01-04 17:35:41 +0100

    read meetup group data and parse into data frame

--- rug-analytics.R
+++ rug-analytics.R
@@ -12,14 +12,46 @@ links <- xpathSApply(doc.html, "//a/@href") %>%
     as.data.frame(stringsAsFactors = FALSE)
 colnames(links) <- c("url")
 
-links %>% 
-    filter(grepl("http://www.meetup.com/", url))
-
-groupName = "Berlin-R-Users-Group"
+# extract respective group names
+links %<>% 
+    filter(grepl("http://www.meetup.com/", 
+                 url)) %>% 
+    mutate(groupName = sub("^http://www.meetup.com/([a-zA-Z0-9-]+?)(/.*|)$", 
+                           "\\1", 
+                           url))
 
 # fetch data from meetup.com via REST API
-APIcall = paste0("https://api.meetup.com/", 
-                 groupName, 
-                 "?&sign=true&photo-host=public&key=", 
-                 APIkey)
-data <- fromJSON(APIcall)
+data <- list()
+i <- 1
+for(groupName in links$groupName) {
+    APIcall = paste0("https://api.meetup.com/", 
+                     groupName, 
+                     "?&sign=true&photo-host=public&key=", 
+                     APIkey)
+    json <- fromJSON(APIcall, flatten = TRUE)
+    data[[i]] <- c(id = json$id, 
+                   name = json$name, 
+                   created = json$created, 
+                   city = json$city, 
+                   country = json$country, 
+                   lat = json$lat, 
+                   lon = json$lon, 
+                   members = json$members)
+    i <- i + 1
+}
+
+# build user group data frame
+userGroups <- data %>% 
+    unlist() %>% 
+    matrix(nrow = nrow(links), 
+           byrow = TRUE) %>% 
+    as.data.frame(stringsAsFactors = FALSE)
+colnames(userGroups) <- c("id", 
+                          "name", 
+                          "created", 
+                          "city", 
+                          "country", 
+                          "lat", 
+                          "lon", 
+                          "members")
+userGroups
